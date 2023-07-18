d010245c1c68b847319f401d2847194b9c55c288
DaianeKelly <Daayanekelly58@gmail.com>
2023-07-17 21:53:16 -0300

    teste fundos

---
+++ estudos/python/ex001/ex001.py
@@ -0,0 +1,10 @@
+print('olá,mundo')
+nome=input('Qual é o seu nome?')
+print('Olá,',nome,',prazer em te conhecer!')
+peso=input('Qual é o seu peso?')
+print('Seu peso é',peso,',',nome,'!')
+
+
+
+
+ 
