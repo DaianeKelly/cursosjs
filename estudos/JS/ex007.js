f2faf6d45b812ca71cf6597d4b3c6dd58c7f611a
DaianeKelly <Daayanekelly58@gmail.com>
2023-08-16 23:42:24 -0300

    js if else teste

---
+++ estudos/JS/ex007.js
@@ -0,0 +1,8 @@
+var vel = 59.5;
+if (vel > 60) {
+  console.log("Você ultrapassou a velocidade permitida, MULTADO(A)!");
+} else {
+  console.log(
+    `A velocidade do seu carro é ${vel}, use cinto de segurança e dirija com cuidado.`
+  );
+}
